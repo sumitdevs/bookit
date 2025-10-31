e8050998ee2bf1e35c86a70f60b792eb97b2c5f6
sumit devs <mysumitdevs@gmail.com>
2025-10-31 23:05:44 +0530

    db connection issue resolved

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import cors from 'cors';
-import morgan from 'morgan';
+import dotenv from 'dotenv';
+// import morgan from 'morgan';
 import experiencesRoutes from './routes/experiences.routes.js';
 import bookingsRoutes from './routes/bookings.routes.js';
 import promoRoutes from './routes/promo.routes.js';
+import connectDB from './config/db.js';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config({ path: '.env' });
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
+
+connectDB();
 
 app.use('/api/experiences', experiencesRoutes);
 app.use('/api/bookings', bookingsRoutes);
@@ -17,4 +24,6 @@ app.use('/api/promo', promoRoutes);
 
 app.get('/', (req, res) => res.json({ ok: true, message: 'BookIt backend' }));
 
-export default app;
+app.listen(PORT, ()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
+});
