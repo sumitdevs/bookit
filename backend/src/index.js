19464eb675fa9878431509c9691c3172dfc30ebb
sumit devs <mysumitdevs@gmail.com>
2025-10-31 21:18:03 +0530

    connection corrected

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,14 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import app from './app.js';
+import connectDB from './config/db.js';
 
 dotenv.config({ path: '.env' });
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
 
-if (!MONGO_URI) {
-  console.error('MONGO_URI missing in .env');
-  process.exit(1);
-}
+connectDB();
 
-mongoose.connect(MONGO_URI).then(() => {
-  console.log('MongoDB connected');
-  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-}).catch(err => {
-  console.error('MongoDB connection error:', err);
-  process.exit(1);
+app.listen(PORT, ()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
 });
